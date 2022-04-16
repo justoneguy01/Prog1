d2e9587128084a822f4fd5fb27f13806ec531e7d
justoneguy01 <90860513+justoneguy01@users.noreply.github.com>
2022-04-16 23:25:56 +0200

    Update drill15_2.cpp

--- drill15/drill15_2.cpp
+++ drill15/drill15_2.cpp
@@ -1,58 +1,97 @@
+//drill15_2(class)
+
+/*
+1. Define a struct Person containing a string name and an int age.
+
+2. Define a variable of type Person, initialize it with “Goofy” and 63, and write it to the screen (cout).
+
+3. Define an input (>>) and an output (<<) operator for Person; read in a Person from the keyboard (cin) and write it out to the screen (cout).
+
+4. Give Person a constructor initializing name and age.
+
+5. Make the representation of Person private, and provide const member functions name() and age() to read the name and age.
+
+6. Modify >> and << to work with the redefined Person.
+
+7. Modify the constructor to check that age is [0:150) and that name doesn’t contain any of the characters ; : " ' [ ] * & ^
+% $ # @ !. Use error() in case of error. Test.
+
+8. Read a sequence of Persons from input (cin) into a vector<Person>; write them out again to the screen (cout). Test
+with correct and erroneous input.
+
+9. Change the representation of Person to have first_name and second_name instead of name. Make it an error not to
+supply both a first and a second name. Be sure to fix >> and << also. Test.*/
+
+//g++ drill15_2.cpp -o drill15_2
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <stdexcept>
 using namespace std;
 
-class Person{
-	public:
-		Person(){};
-		Person(string f, string l, int a) : f{f}, l{l}, a{a}{
-			if (a < 0 || a >=150)
-			{
-				throw runtime_error ("Invalid age!");
-			}
-			string n = f + l;
-			for (int i = 0; i < n.length(); ++i)
-			{
-				if (n[i] == ';' || n[i] == ':' || n[i] == '"' || n[i] == '[' ||  n[i] == 96 || n[i] == ']' || n[i] == '*' || n[i] == '&' || n[i] =='^' || n[i] == '%' || n[i] == '$' || n[i] == '#' || n[i] == '@' || n[i] == '!')
-				{
-					throw runtime_error ("Invalid name!");
-				}
-			}
-		};
-		string first() const{return f;}
-		string last() const{return l;}
-		int age() const{return a;}
+struct Person
+{
 	private:
-		//string n;
+
 		string f;
 		string l;
 		int a;
+
+	public:
+	 	Person(){};
+	    Person (string pFirst, string pLast, int pAge)
+	    {
+	    	if (pAge < 0 || pAge > 150)
+	    	{
+	    		throw runtime_error("invalid age");
+	    	}
+	    	string n = pFirst + pLast;
+	    	for (int i = 0; i < n.length(); i++)
+	    	{
+	    		if (n[i] == ';' || n[i] == ':' || n[i] == '"' || n[i] == '[' || n[i] == ']' || n[i] == '*' || n[i] == '&' || n[i] =='^' || n[i] == '%' || n[i] == '$' || n[i] == '#' || n[i] == '@' || n[i] == '!')
+				{
+					throw runtime_error ("Invalid name!");
+				}
+	    	}
+	    	f=pFirst;
+		    l=pLast;
+		    a=pAge;
+	    }
+		string first()const{return f;}
+		string last()const{return l;}
+		int age()const{return a;}
+	
 };
 
-ostream& operator<< (ostream& os, const Person& p){return os << p.first() << " " << p.last() << " " << p.age();}
-istream& operator>> (istream& is, Person& p){
+ostream& operator<< (ostream& os, const Person& p)
+{
+	return os << p.first() << " " << p.last()<< " " << p.age()<<endl;
+}
+
+istream& operator>> (istream& is, Person& p)
+{
 	string f;
 	string l;
 	int a;
 	is >> f >> l >> a;
-	p=Person(f, l, a);
+	p=Person(f,l,a);
 	return is;
 }
 
 int main(){
-
+	/*
 	Person p1;
-	//p1.n = "Goofy";
-	//p1.a = 63;
-	p1 = Person("Goofy", "Goofy", 63);
-	cout << p1.first() << " " << p1.last() << " " << p1.age() << endl;
+	p1.name = "Goofy";
+	p1.age = 63;*/
 
+	Person p1 {"Goofy","Goofy",63};
+	cout << "Name: " << p1.first()<<" "<< p1.last() << " Age: " << p1.age() << endl;
+	
 	Person p2;
 	Person p3;
 	cin >> p2 >> p3;
-	cout << p2 << " " << p3 << endl;
+	cout << p2 << p3;
 
 	vector<Person> vec;
 
@@ -66,5 +105,4 @@ int main(){
 	{
 		cout << p << endl;
 	}
-
 }
