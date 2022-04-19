3187b138f9ce8ce911c9afcc5ed03d033067030c
justoneguy01 <90860513+justoneguy01@users.noreply.github.com>
2022-04-19 11:42:53 +0200

    Update drill19.cpp

--- drill19/drill19.cpp
+++ drill19/drill19.cpp
@@ -1,36 +1,72 @@
+//drill19
+
+/*
+1. Define template<typename T> struct S { T val; };.
+
+2. Add a constructor, so that you can initialize with a T.
+
+3. Define variables of types S<int>, S<char>, S<double>, S<string>, and S<vector<int>>; initialize them with values of your choice.
+
+4. Read those values and print them.
+
+5. Add a function template get() that returns a reference to val.
+
+6. Put the definition of get() outside the class.
+
+7. Make val private.
+
+8. Do 4 again using get().
+
+9. Add a set() function template so that you can change val.
+
+10. Replace set() with an S<T>::operator=(const T&). Hint: Much simpler than §19.2.5.
+
+11. Provide const and non-const versions of get().
+
+12. Define a function template<typename T> read_val(T& v) that reads from cin into v.
+
+13. Use read_val() to read into each of the variables from 3 except the S<vector<int>> variable.
+
+14. Bonus: Define input and output operators (>> and <<) for vector<T>s. For both input and output use a { val, val, val }
+format. That will allow read_val() to also handle the S<vector<int>> variable. Remember to test after each step.*/
+
+//g++ drill19.cpp -o drill19
+
 #include <iostream>
 #include <string>
 #include <vector>
-
 using namespace std;
 
 template<typename T>
 struct S {
-	S (T vv = 0) : val{vv}{};
-	T& get();
-	void set(T new_t) {val = new_t;}
-	void operator=(const T& S);
-	const T& get() const;
-private:
-	T val;
+	public:
+		
+		S (T vv = 0) : val{vv}{}; 			
+		T& get();
+		void set(T new_val){val=new_val;}	//set
+		void operator=(const T& S);			//operator= 
+		const T& get() const;				//const get 
+	private: 
+		
+		T val;
 };
 
-template<typename T>
+template<typename T>					//get
 T& S<T>::get(){
 	return val;
 }
 
-template<typename T>
+template<typename T>					//get
 const T& S<T>::get() const{
 	return val;
 }
 
-template<typename T>
+template<typename T>					//operator= 
 void S<T>::operator=(const T& s){
 	val = s;
 }
 
-template<typename T>
+template<typename T>					//read
 void read_val(T& v){
 	cin >> v;
 }
@@ -69,44 +105,42 @@ istream& operator>>(istream& is, vector<T>& v){
 }
 
 int main(){
-	
-	S<int> s;
+
 	S<int> si {37};
 	S<char> sc {'c'};
 	S<double> sd {3.2};
-	S<string> ss{"Hello"};
+	S<string> ss {"Hello"};
 	S<vector<int>> svi {vector<int>{1,1,2,3,5,8}};
 
-	cout << "S<int> : " << s.get() << endl;
 	cout << "S<int> : " << si.get() << endl;
 	cout << "S<char> : " << sc.get() << endl;
 	cout << "S<double> : " << sd.get() << endl;
 	cout << "S<string> : " << ss.get() << endl;
-	//cout << "S<vector<int>> : " << svi.val << endl;
-
 	cout << "S<vector<int>> : ";
-
-	for (auto& a: svi.get())
+	for (auto& a : svi.get())
 	{
 		cout << a << ' ';
 	}
-
 	cout << endl;
 
-	sc.set('s');
+	//set
+	sc.operator=('s');
 	cout << "S<char> : " << sc.get() << endl;
 
-	sd = 42.1;
+	sd.operator=( 42.1);
 	cout << "S<double> : " << sd.get() << endl;
 
+	cout << "\nEnter an intiger: " <<endl;
 	int ii;
 	read_val(ii);
 	S<int> si2 {ii};
 
+	cout << "\nEnter a Double: " <<endl;
 	double dd;
 	read_val(dd);
 	S<double> sd2 {dd};
 
+	cout << "\nEnter a string " <<endl;
 	string ss2;
 	read_val(ss2);
 	S<string> str {ss2};
@@ -115,11 +149,9 @@ int main(){
 	cout << "S<double> : " << sd2.get() << endl;
 	cout << "S<string> : " << str.get() << endl;
 
-	cout << "Add a vector in this format {val, val, val}: ";
-	vector<int> vint;
-	read_val(vint);
-	S<vector<int>> svi2 {svi2};
-
-	cout << "S<vector<int>> : " << svi.get() << endl;
-
+    cout << "Vector<int>: (format: { val1, val2, val3 }) ";
+    vector<int> vi2;
+    read_val(vi2);
+    S<vector<int>> svi2 {vi2};
+	cout << "S<vector<int>> read: " << svi2.get() << '\n';
 }
