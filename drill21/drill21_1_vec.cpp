64221f9a845b35ee0e51e9dda650610c2964a903
justoneguy01 <90860513+justoneguy01@users.noreply.github.com>
2022-04-19 21:19:43 +0200

    Update drill21_1_vec.cpp

--- drill21/drill21_1_vec.cpp
+++ drill21/drill21_1_vec.cpp
@@ -1,137 +1,181 @@
 
+/*
+1. Define a struct Item { string name; int iid; double value; /. . . . ./ };, 
+make a vector<Item>, vi, and fill it withten items from a file.
+
+2. Sort vi by name.
+
+3. Sort vi by iid.
+
+4. Sort vi by value; print it in order of decreasing value (i.e., largest value first).
+
+5. Insert Item("horse shoe",99,12.34) and Item("Canon S400", 9988,499.95).
+
+6. Remove (erase) two Items identified by name from vi.
+
+7. Remove (erase) two Items identified by iid from vi.
+
+8. Repeat the exercise with a list<Item> rather than a vector<Item>*/
+
+//g++ drill21_1_vec.cpp -o drill21_1_vec
+
 #include <iostream>
-#include <stdexcept>
-#include <string>
 #include <vector>
 #include <fstream>
+#include <algorithm>
+#include <iterator>
+#include <string>
 #include <list>
+using namespace std;
 
 struct Item {
-    std::string name;
-    int iid;
+    string name; 
+    int iid; 
     double value;
 };
 
-std::ostream& operator<<(std::ostream& os, const Item& it)
-{
-    return os << it.name << '\t'
-              << it.iid << '\t'
-              << it.value;
+ostream& operator<<(ostream& os, Item& item){
+        os << item.name << " " << item.iid << " " << item.value << endl;
+    return os;
 }
 
-std::istream& operator>>(std::istream& is, Item& it)
-{
-    std::string ss;
-    int ii;
-    double dd;
-    is >> ss >> ii >> dd;
-    Item itt {ss, ii, dd};
-    it = itt;
+istream& operator>>(istream& is, Item& item){
+    string n; 
+    int i; 
+    double d;
+    is >> n >> i >> d;
+    Item it{n,i,d};
+    item = it;
     return is;
 }
 
-struct Cmp_by_dec_val {
+struct sort_by_name{
+    bool operator()(const Item& a, const Item& b)
+    {
+        return a.name < b.name;
+    }
+};
+struct sort_by_iid{
+    bool operator()(const Item& a, const Item& b)
+    {
+        return a.iid < b.iid;
+    }
+};
+struct sort_by_value_desc{
     bool operator()(const Item& a, const Item& b)
     {
         return a.value > b.value;
     }
 };
 
-class Smaller_than {
-    double val;
+class find_name{
+private:
+    string name;
 public:
-    Smaller_than(double vv) : val{vv} { }
-    bool operator()(const Item& x) const { return x.value < val; }
-};
-
-class Cmp_by_name {
-    std::string name;
-public:
-    Cmp_by_name(const std::string& nn) : name{nn} { }
+    find_name(const string& nn){name=nn;} //: name{nn} { }
     bool operator()(const Item& x) const { return x.name == name; }
 };
 
-class Cmp_by_iid {
+class find_iid{
+private:
     int iid;
 public:
-    Cmp_by_iid(int id) : iid{id} { }
+    find_iid(int id){iid=id;} //: iid{id} { }
     bool operator()(const Item& x) const { return x.iid == iid; }
 };
 
-template<typename C>
-void print(const C& c, char sep = ' ')
+
+template<typename T>
+void print_list(T& t)
 {
-    for (const auto& a : c)
-        std::cout << a << sep;
-    std::cout << '\n';
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    for (auto& a : t)
+    {
+        cout << a;
+    }
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~" << endl;
 }
 
-int main()
-try {
-    const std::string iname {"input_items.txt"};
+template <typename T>
+void print_vector(T& t){
 
-    std::ifstream ifs {iname};
-    if (!ifs) throw std::runtime_error("Could not read from file: " + iname);
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    for (int i = 0; i < t.size(); ++i){
+        cout << t[i];
+    }
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+}
 
-    std::vector<Item> vi;
-    for (Item ii; ifs >> ii; )
-        vi.push_back(ii);
+int main(){
+    
+    vector<Item> vi;
 
-    std::list<Item> li (vi.size());
-    std::copy(vi.begin(), vi.end(), li.begin());
+    ifstream file;
+    file.open("file.txt");
 
-    std::sort(vi.begin(), vi.end(),
-            [](const Item& a, const Item& b) { return a.name < b.name; });
+    for (Item i ; file >> i;)
+    {
+        vi.push_back(i);
+    }
+    cout<<"~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+    cout<<"\nDefault"<<endl<<endl;
+    print_vector(vi);
 
-    std::sort(vi.begin(), vi.end(),
-            [](const Item& a, const Item& b) { return a.iid < b.iid; });
+    //8.
+    list<Item> li (vi.size());
+    copy(vi.begin(), vi.end(), li.begin());
 
-    std::sort(vi.begin(), vi.end(), Cmp_by_dec_val());
+    //2.
+    cout<<"\nSort vi by name"<<endl<<endl;    
+    sort(vi.begin(), vi.end(),sort_by_name());
+    print_vector(vi);
 
-    Item tmp {"horse shoe", 99, 1.34};  
-    vi.insert(
-            std::find_if(vi.begin(), vi.end(), Smaller_than(tmp.value)),
-            tmp);
+    //3.
+    cout<<"\nSort vi by iid"<<endl<<endl; 
+    sort(vi.begin(), vi.end(),sort_by_iid());
+    print_vector(vi);
 
-    tmp = Item{"Canon S400", 9988, 499.95};
-    vi.insert(
-            std::find_if(vi.begin(), vi.end(), Smaller_than(tmp.value)),
-            tmp);
+    //4.
+    cout<<"\nSort vi by value (desc)"<<endl<<endl; 
+    sort(vi.begin(), vi.end(),sort_by_value_desc());
+    print_vector(vi);
 
-    vi.erase(std::find_if(vi.begin(), vi.end(), Cmp_by_name("water")));
-    vi.erase(std::find_if(vi.begin(), vi.end(), Cmp_by_name("chair")));
+    //5.
+    cout<<"\nAdding new elements"<<endl<<endl; 
+    vi.push_back(Item{"horse shoe", 99, 12.34});
+    vi.push_back(Item{"Canon S400", 9988, 499.95});
+    print_vector(vi);
 
-    vi.erase(std::find_if(vi.begin(), vi.end(), Cmp_by_iid(17)));
-    vi.erase(std::find_if(vi.begin(), vi.end(), Cmp_by_iid(83)));
+    //6
+    cout<<"\nErasing two items identified by name"<<endl<<endl; 
+    vi.erase(find_if(vi.begin(), vi.end(), find_name("table")));
+    vi.erase(find_if(vi.begin(), vi.end(), find_name("chair")));
+    print_vector(vi);
 
-    print(li, '\n');
+    //7.
+    cout<<"\nErasing two items identified by iid"<<endl<<endl; 
+    vi.erase(find_if(vi.begin(), vi.end(), find_iid(17)));
+    vi.erase(find_if(vi.begin(), vi.end(), find_iid(83)));
+    print_vector(vi);
 
+    //8.
+    cout<<"\nCopying to the list and repeating the exercise"<<endl<<endl; 
+    print_list(li);
+    // sort with lambda as predicate
+    // list sort is member function since list does not have a random-access
+    // iterator as needed by std::sort.
     li.sort([](const Item& a, const Item& b) { return a.name < b.name; });
     li.sort([](const Item& a, const Item& b) { return a.iid < b.iid; });
     li.sort([](const Item& a, const Item& b) { return a.value > b.value; });
 
-    tmp = {"horse", 99, 1.34};
-    li.insert(std::find_if(li.begin(), li.end(), Smaller_than(tmp.value)),
-            tmp);
+    li.push_back(Item{"horse shoe", 99, 12.34});
+    li.push_back(Item{"Canon S400", 9988, 499.95});
 
-    tmp = {"Canon", 9988, 499.95};
-    li.insert(std::find_if(li.begin(), li.end(), Smaller_than(tmp.value)),
-            tmp);
+    li.erase(find_if(li.begin(), li.end(), find_name("table")));
+    li.erase(find_if(li.begin(), li.end(), find_name("chair")));
 
-    li.erase(std::find_if(li.begin(), li.end(), Cmp_by_name("table")));
-    li.erase(std::find_if(li.begin(), li.end(), Cmp_by_name("phone")));
-
-    li.erase(std::find_if(li.begin(), li.end(), Cmp_by_iid(58)));
-    li.erase(std::find_if(li.begin(), li.end(), Cmp_by_iid(2011)));
-
-    print(li, '\n');
+    li.erase(find_if(li.begin(), li.end(), find_iid(17)));
+    li.erase(find_if(li.begin(), li.end(), find_iid(83)));
+    cout<<"\nThe final result"<<endl<<endl;
+    print_list(li);
 }
-catch(std::exception& e) {
-    std::cerr << "Exception: " << e.what() << '\n';
-    return 1;
-}
-catch(...) {
-    std::cerr << "Unknown exception\n";
-    return 2;
-}
-
